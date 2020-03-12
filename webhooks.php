9476285de33391bc1b8fddaaa4ab6100d760f6fe
princezfin <62057050+princezfin@users.noreply.github.com>
2020-03-12 08:33:27 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -1,8 +1,10 @@
-
 <?php
 require "vendor/autoload.php";
 
+$access_token = '
 $access_token = 'l3RxPBy92zqghEeGffAUkwacjB2kWCHmat1qjNyTVGEAqQfXwe8V46/ldjWX2JFv2WE4SJik3jWXYUQwADgGLYfNwlQEkNkfvLYONyshhLjnbWkp2LFUllyKFFU8dTvNTVQgRrwSrKU2SkdMgRbg+wdB04t89/1O/w1cDnyilFU=';
+$channelSecret = '2c25e4fd85b46b0ba27f6f6af9a01f0c';
+$idPush = 'U805de99b9cbb3087982f443d64caac9e'';
 $channelSecret = '2c25e4fd85b46b0ba27f6f6af9a01f0c';
 $idPush = 'U805de99b9cbb3087982f443d64caac9e'
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
