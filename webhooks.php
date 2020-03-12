0d24171b144d69fe9bf1275c4c889a9662cf1514
princezfin <62057050+princezfin@users.noreply.github.com>
2020-03-12 08:31:15 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -1,51 +1,16 @@
-<?php // callback.php
 
+<?php
 require "vendor/autoload.php";
-require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
 $access_token = 'l3RxPBy92zqghEeGffAUkwacjB2kWCHmat1qjNyTVGEAqQfXwe8V46/ldjWX2JFv2WE4SJik3jWXYUQwADgGLYfNwlQEkNkfvLYONyshhLjnbWkp2LFUllyKFFU8dTvNTVQgRrwSrKU2SkdMgRbg+wdB04t89/1O/w1cDnyilFU=';
+$channelSecret = '2c25e4fd85b46b0ba27f6f6af9a01f0c';
+$idPush = 'U805de99b9cbb3087982f443d64caac9e'
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('น้องผู้ช่วยคุณ Cha-vee กำลังเรียนรู้อยู่ขอเวลาอีกนิดน้า');
+$response = $bot->pushMessage($idPush, $textMessageBuilder);
 
-// Get POST body content
-$content = file_get_contents('php://input');
-// Parse JSON
-$events = json_decode($content, true);
-// Validate parsed JSON data
-if (!is_null($events['events'])) {
-	// Loop through each event
-	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
-			$text = $event['source']['userId'];
-			// Get replyToken
-			$replyToken = $event['replyToken'];
+echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $text
-			];
 
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-
-			echo $result . "\r\n";
-		}
-	}
-}
-echo "OK";
